c3f43582fa031c887b361f0965d0d31c2f6025ab
Diana Luise <dianaluise24@gmail.com>
2022-08-25 21:22:58 -0300

    PRATICA 02 TERCEIRO DIA

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+// Tabuada Nove
+const multiplicador = 9
+let i = 1
+
+while (i <= 10) {
+    const  resultado = i*multiplicador
+    const  valorinserido = `${multiplicador} X ${i} = ${resultado}`
+    console.log(valorinserido)
+    i++
+}
